c0df9bb5eb47a8e7b2678a0c942f9a95708ef46f
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-07-13 14:51:18 +0100

    Update

--- lib/models/tracks.dart
+++ lib/models/tracks.dart
@@ -1,13 +1,23 @@
-class Track {
+class SongData {
   final String title;
   final String artist;
   final String timestamp;
   final String coverArt;
 
-  Track(
+  SongData(
     this.title,
     this.artist,
     this.timestamp,
     this.coverArt,
+  );
+}
+
+class AlbumData {
+  String title;
+  List<SongData> songList;
+
+  AlbumData(
+    this.title,
+    this.songList,
   );
 }
