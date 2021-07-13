5ae48b12e5990c8f51e893359cb3296ecc62475b
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-07-13 14:55:23 +0100

    Update

---
+++ lib/screens/tabs/Songs/song_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:music_streaming/constants/common.dart';
+
+class Songs extends StatefulWidget {
+  const Songs({Key? key}) : super(key: key);
+
+  @override
+  _SongsState createState() => _SongsState();
+}
+
+class _SongsState extends State<Songs> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, top: 30, right: 30),
+      child: Column(
+        children: [
+          ...List.generate(5, (index) {
+            return SongTile(
+              songTitle: 'Bebo',
+              artist: 'Burna boy, Lexus , Joey B , Joey B ,Joey B ',
+              timestamp: '4:30',
+              onTap: () {},
+            );
+          })
+        ],
+      ),
+    );
+  }
+}
