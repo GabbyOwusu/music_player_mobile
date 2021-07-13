8949a57f089051817a3bc83decf3201609c31a19
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-07-13 14:54:55 +0100

    Added albums screen

---
+++ lib/screens/tabs/Albums/album_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Albums extends StatefulWidget {
+  const Albums({Key? key}) : super(key: key);
+
+  @override
+  _AlbumsState createState() => _AlbumsState();
+}
+
+class _AlbumsState extends State<Albums> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
