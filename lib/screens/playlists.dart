2fd0a4e3e40eae6eac1e9d6b0b29c31b49d397e8
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-07-18 17:21:33 +0100

    Added playlist screen

---
+++ lib/screens/playlists.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:music_streaming/constants/common.dart';
+
+class Playlist extends StatefulWidget {
+  const Playlist({Key key}) : super(key: key);
+
+  @override
+  _PlaylistState createState() => _PlaylistState();
+}
+
+class _PlaylistState extends State<Playlist> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBAr(
+        context,
+        'Playlists',
+        Icon(Icons.sort, color: Colors.black),
+        backbutton(context),
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Just for you',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Created a list of mixes just for you',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 13,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 250,
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 180,
+                    width: 350,
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    margin: EdgeInsets.only(
+                      left: index == 0 ? 20 : 0,
+                      right: 20,
+                      bottom: 60,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          offset: Offset(0, 30),
+                          blurRadius: 30,
+                          spreadRadius: -15,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Text('45mins'),
+                        ),
+                        Spacer(),
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Burna boy,Joey B',
+                                  style: TextStyle(fontSize: 13),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  'Level up',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18,
+                                  ),
+                                )
+                              ],
+                            ),
+                            Spacer(),
+                            playArrow,
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            // SizedBox(height: 80),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Playlist',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'All your playlists in one place',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 13,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            Container(
+              height: 2000,
+              padding: const EdgeInsets.only(
+                left: 20,
+                top: 30,
+                right: 20,
+                bottom: 10,
+              ),
+              child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 3,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisExtent: 270,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 20,
+                ),
+                itemBuilder: (context, index) {
+                  return index == 0
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 200,
+                              height: 200,
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Icon(Icons.add),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Create playlist',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              'Create a new playlist',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        )
+                      : GestureDetector(
+                          onTap: () {},
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 200,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                child: Icon(Icons.music_note),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Playlist title',
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                'Number of songs 10',
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
