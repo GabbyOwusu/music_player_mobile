1de70bf8ff9b4af54396bccc350b47b0e1e31092
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2022-03-13 02:39:51 +0000

    Update song_screen.dart

--- lib/screens/tabs/Songs/song_screen.dart
+++ lib/screens/tabs/Songs/song_screen.dart
@@ -1,129 +1,46 @@
-import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_audio_query/flutter_audio_query.dart';
-
-import 'package:music_streaming/constants/common.dart';
 import 'package:music_streaming/providers/songs_provider.dart';
+import 'package:music_streaming/screens/tabs/Songs/song_tile.dart';
 import 'package:provider/provider.dart';
 
 class Songs extends StatefulWidget {
-  const Songs({Key key}) : super(key: key);
+  const Songs({Key? key}) : super(key: key);
 
   @override
   _SongsState createState() => _SongsState();
 }
 
-class _SongsState extends State<Songs> {
-  int currentIndex;
-  SongProvider get p {
-    return Provider.of<SongProvider>(context, listen: false);
-  }
-
-  FlutterAudioQuery query = FlutterAudioQuery();
+class _SongsState extends State<Songs> with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
-    context.watch<SongProvider>().songs;
-    return p.songs.length != null
-        ? Padding(
-            padding: const EdgeInsets.only(left: 10, right: 30),
-            child: Column(
-              children: [
-                Container(
-                  height: 700,
-                  child: ListView.builder(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: p.songs.length,
-                    itemBuilder: (context, index) {
-                      currentIndex = index;
-                      return Stack(
-                        children: [
-                          if (p.androidversion > 28)
-                            BuildSongList(index: index, p: p)
-                          else
-                            SongTile(
-                              coverArt: p.songs[index].albumArtwork == null
-                                  ? AssetImage('images/music_note.png')
-                                  : FileImage(
-                                      File(p.songs[index].albumArtwork),
-                                    ),
-                              index: index,
-                              provider: p,
-                              songInfo: p.songs[index],
-                              onTap: () async {
-                                setState(() {
-                                  p.playSong(p.songs[index]);
-                                });
-                              },
-                            ),
-                        ],
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          )
-        : Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.music_note_rounded),
-                Text('There are no songs on your device'),
-              ],
-            ),
-          );
-  }
-}
+    super.build(context);
+    final provider = context.watch<SongProvider>();
+    final songs = provider.songs;
 
-class BuildSongList extends StatefulWidget {
-  final SongProvider p;
-  final int index;
-  const BuildSongList({
-    Key key,
-    @required this.index,
-    @required this.p,
-  }) : super(key: key);
-
-  @override
-  _BuildSongListState createState() => _BuildSongListState();
-}
-
-class _BuildSongListState extends State<BuildSongList> {
-  Future data;
-
-  @override
-  void initState() {
-    data = widget.p.audioQuery.getArtwork(
-      type: ResourceType.SONG,
-      id: widget.p.songs[widget.index].id,
+    return SizedBox(
+      height: 500,
+      child: ListView.separated(
+        separatorBuilder: (context, index) => SizedBox(height: 5),
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+        shrinkWrap: true,
+        physics: BouncingScrollPhysics(),
+        itemCount: songs.length,
+        itemBuilder: (context, index) {
+          return SongTile(song: songs[index]);
+        },
+      ),
     );
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<Uint8List>(
-        future: data,
-        builder: (context, snapshot) {
-          return snapshot.connectionState == ConnectionState.done
-              ? SongTile(
-                  coverArt: snapshot.data.isEmpty || snapshot.data == null
-                      ? AssetImage('images/song_note.png')
-                      : MemoryImage(snapshot?.data),
-                  index: widget.index,
-                  provider: widget.p,
-                  songInfo: widget.p.songs[widget.index],
-                  onTap: () async {
-                    setState(() {
-                      widget.p.playSong(widget.p.songs[widget.index]);
-                    });
-                  },
-                )
-              : SizedBox();
-        });
-    ;
+    // : Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         Icon(Icons.music_note_rounded),
+    //         Text('There are no songs on your device'),
+    //       ],
+    //     ),
+    //   );
   }
 }
