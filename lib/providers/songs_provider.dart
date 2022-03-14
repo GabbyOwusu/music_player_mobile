2349c2edb458e36f7dc6766fa7504c08b2410470
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2022-03-14 09:57:44 +0000

    Update songs_provider.dart

--- lib/providers/songs_provider.dart
+++ lib/providers/songs_provider.dart
@@ -1,44 +1,45 @@
 import 'dart:io';
-
 import 'package:music_streaming/providers/base_provider.dart';
-import 'package:music_streaming/services/device_info_service.dart';
+import 'package:music_streaming/services/hice_service.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:just_audio/just_audio.dart';
 
 class SongProvider extends Baseprovider {
+  HiveService _hiveService = HiveService();
   OnAudioQuery audioQuery = OnAudioQuery();
-  DeviceInfoService infoService = DeviceInfoService();
   AudioPlayer audioPlayer = AudioPlayer();
-  // AudioCache audioCache = AudioCache();
 
-  List<SongModel> _songList = [];
+  List<SongModel>? _songList = [];
+  SongModel? _recent;
   Duration? _duration;
   int _currentDuration = 0;
-  List<SongModel> _playListSongs = [];
-  List<AlbumModel> _albumList = [];
-  List<SongModel> _albumSongs = [];
-  List<ArtistModel> _artistList = [];
-  List<PlaylistModel> _playList = [];
+  List<SongModel>? _playListSongs = [];
+  List<AlbumModel>? _albumList = [];
+  List<SongModel>? _albumSongs = [];
+  List<ArtistModel>? _artistList = [];
+  List<PlaylistModel>? _playList = [];
   bool isPlaying = false;
   String? _timePlayed;
   int? _version;
   SongModel? _nowPlaying;
 
-  List<SongModel> get songs => _songList;
-  List<AlbumModel> get albums => _albumList;
-  List<SongModel> get albumSongs => _albumSongs;
-  List<PlaylistModel> get playlist => _playList;
-  List<ArtistModel> get artists => _artistList;
-  List<SongModel> get playlistSongs => _playListSongs;
+  List<SongModel>? get songs => _songList;
+  List<AlbumModel>? get albums => _albumList;
+  List<SongModel>? get albumSongs => _albumSongs;
+  List<PlaylistModel>? get playlist => _playList;
+  List<ArtistModel>? get artists => _artistList;
+  List<SongModel>? get playlistSongs => _playListSongs;
   SongModel? get playing => _nowPlaying;
   int? get androidversion => _version;
   String? get timePlayed => _timePlayed;
   int get currentDuration => _currentDuration;
   Duration? get duration => _duration;
+  SongModel? get recent => _recent;
 
   Future<void> initQuery() async {
     await requestPermission();
-    getSongs();
+    await getSongs();
+    await getRecent();
     getAlbums();
     getArtists();
     getPlayLists();
@@ -77,17 +78,17 @@ class SongProvider extends Baseprovider {
     }
   }
 
-  Future<void> getPlaylistSongs(String id) async {
+  Future<List<SongModel>?> getPlaylistSongs(String id) async {
     try {
       List<SongModel> songs = await audioQuery.queryAudiosFrom(
         AudiosFromType.PLAYLIST,
         id,
       );
       _playListSongs = songs;
-
       notifyListeners();
+      return _playListSongs;
     } catch (e) {
-      return;
+      return null;
     }
   }
 
@@ -98,7 +99,6 @@ class SongProvider extends Baseprovider {
     try {
       List<SongModel> songs = await audioQuery.queryAudiosFrom(type, id);
       _albumSongs = songs;
-
       notifyListeners();
       return _albumSongs;
     } catch (e) {
@@ -110,7 +110,6 @@ class SongProvider extends Baseprovider {
     try {
       List<ArtistModel> artists = await audioQuery.queryArtists();
       _artistList = artists;
-
       notifyListeners();
       return artists;
     } catch (e) {
@@ -128,7 +127,26 @@ class SongProvider extends Baseprovider {
     }
   }
 
+  Future<void> setRecent(SongModel song) async {
+    try {
+      await _hiveService.setRecent(song.id.toString());
+    } catch (e) {}
+  }
+
+  Future<SongModel?> getRecent() async {
+    try {
+      final res = await _hiveService.getRecent();
+      _recent = _songList?.where((s) => res == s.id.toString()).toList().first;
+      notifyListeners();
+      return _recent;
+    } catch (e) {
+      return null;
+    }
+  }
+
   Future<void> playSong(SongModel song) async {
+    await setRecent(song);
+    await loopMode();
     if (song.title == _nowPlaying?.title && song.artist == _nowPlaying?.artist)
       return;
     isPlaying = true;
@@ -145,6 +163,10 @@ class SongProvider extends Baseprovider {
     );
   }
 
+  Future<void> loopMode() async {
+    await audioPlayer.setLoopMode(LoopMode.one);
+  }
+
   Future<void>? pauseSong() async {
     isPlaying = false;
     notifyListeners();
@@ -158,17 +180,24 @@ class SongProvider extends Baseprovider {
   }
 
   Future<void> stopPlaying() async {
+    isPlaying = false;
     notifyListeners();
     return await audioPlayer.stop();
   }
 
-  void getPosition() {
+  void getPosition() async {
     audioPlayer.positionStream.listen(
       (event) async {
         _currentDuration = event.inSeconds;
         _timePlayed = '${event.inMinutes}:${event.inSeconds % 60}';
         notifyListeners();
       },
-    ).onError((error) => print('Error: $error'));
+    )..onError((error) => print('Error: $error'));
+    // ..onDone(() {
+    //   isPlaying = false;
+    //   _currentDuration = 0;
+    //   _timePlayed = '00:00';
+    //   notifyListeners();
+    // });
   }
 }
