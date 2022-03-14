5c223d0b1d791031b5b9d0732b24c74224c93caa
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2022-03-14 10:00:26 +0000

    Create hice_service.dart

---
+++ lib/services/hice_service.dart
@@ -0,0 +1,33 @@
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HiveKeys {
+  static const BOX = "music_player";
+  static const RECENT = "recent_played";
+}
+
+class HiveService {
+  Box? _box;
+
+  Future<void> assertBox() async {
+    if (_box != null && (_box?.isOpen ?? false)) return;
+    return await init();
+  }
+
+  Future<void> init() async {
+    final dir = await getApplicationDocumentsDirectory();
+    Hive..init(dir.path);
+    await Hive.openBox(HiveKeys.BOX).then((b) => _box = b);
+  }
+
+  Future<void> setRecent(String data) async {
+    await assertBox();
+    return await _box?.put(HiveKeys.RECENT, data);
+  }
+
+  Future<String?> getRecent() async {
+    await assertBox();
+    final res = await _box?.get(HiveKeys.RECENT);
+    return res;
+  }
+}
