2c1a585787ae9fc3d50590bc7a3fe92d3e614c33
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2022-04-02 11:34:01 +0000

    [provider] add loop, shuffle, skip and interruption handling

--- lib/providers/songs_provider.dart
+++ lib/providers/songs_provider.dart
@@ -1,48 +1,70 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:audio_session/audio_session.dart';
 import 'package:music_streaming/providers/base_provider.dart';
-import 'package:music_streaming/services/hice_service.dart';
+import 'package:music_streaming/services/hive_service.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:just_audio/just_audio.dart';
+
+enum Loop { repeat, all }
 
 class SongProvider extends Baseprovider {
   HiveService _hiveService = HiveService();
   OnAudioQuery audioQuery = OnAudioQuery();
-  AudioPlayer audioPlayer = AudioPlayer();
+  AudioPlayer? audioPlayer;
 
-  List<SongModel>? _songList = [];
+  List<SongModel> _songList = [];
+  bool? _shuffleMode;
+  int _currentIndex = 0;
+  Uint8List? _nowPlayingArt;
   SongModel? _recent;
   Duration? _duration;
   int _currentDuration = 0;
-  List<SongModel>? _playListSongs = [];
-  List<AlbumModel>? _albumList = [];
-  List<SongModel>? _albumSongs = [];
-  List<ArtistModel>? _artistList = [];
-  List<PlaylistModel>? _playList = [];
+  List<SongModel> _nowPlayingList = [];
+  List<SongModel> _playListSongs = [];
+  List<AlbumModel> _albumList = [];
+  List<SongModel> _albumSongs = [];
+  List<ArtistModel> _artistList = [];
+  List<PlaylistModel> _playList = [];
   bool isPlaying = false;
-  String? _timePlayed;
-  int? _version;
   SongModel? _nowPlaying;
+  Loop _currentLoop = Loop.all;
 
-  List<SongModel>? get songs => _songList;
-  List<AlbumModel>? get albums => _albumList;
-  List<SongModel>? get albumSongs => _albumSongs;
-  List<PlaylistModel>? get playlist => _playList;
-  List<ArtistModel>? get artists => _artistList;
-  List<SongModel>? get playlistSongs => _playListSongs;
+  List<SongModel> get songs => _songList;
+  List<AlbumModel> get albums => _albumList;
+  List<SongModel> get albumSongs => _albumSongs;
+  List<PlaylistModel> get playlist => _playList;
+  List<ArtistModel> get artists => _artistList;
+  List<SongModel> get playlistSongs => _playListSongs;
   SongModel? get playing => _nowPlaying;
-  int? get androidversion => _version;
-  String? get timePlayed => _timePlayed;
   int get currentDuration => _currentDuration;
   Duration? get duration => _duration;
   SongModel? get recent => _recent;
+  Uint8List? get nowPlayingArt => _nowPlayingArt;
+  bool? get isRepeat => _currentLoop == Loop.repeat;
+  bool? get shuffle => _shuffleMode;
+  Loop get loop => _currentLoop;
 
   Future<void> initQuery() async {
     await requestPermission();
-    await getSongs();
     await getRecent();
-    getAlbums();
-    getArtists();
-    getPlayLists();
+    await getSongs();
+    await getAlbums();
+    await getArtists();
+    await getPlayLists();
+    await recentArt();
+  }
+
+  Future<void> recentArt() async {
+    final a = await audioQuery.queryArtwork(
+      _recent?.id ?? 0,
+      ArtworkType.AUDIO,
+      size: 800,
+    );
+    _nowPlayingArt = a;
+    notifyListeners();
   }
 
   Future<List<SongModel>?> getSongs() async {
@@ -80,7 +102,7 @@ class SongProvider extends Baseprovider {
 
   Future<List<SongModel>?> getPlaylistSongs(String id) async {
     try {
-      List<SongModel> songs = await audioQuery.queryAudiosFrom(
+      final songs = await audioQuery.queryAudiosFrom(
         AudiosFromType.PLAYLIST,
         id,
       );
@@ -104,6 +126,20 @@ class SongProvider extends Baseprovider {
     } catch (e) {
       return null;
     }
+  }
+
+  Future<Uint8List?> artWork({
+    required int id,
+    required ArtworkType type,
+  }) async {
+    final res = await audioQuery.queryArtwork(
+      id,
+      ArtworkType.AUDIO,
+      format: ArtworkFormat.JPEG,
+      size: 600,
+      quality: 100,
+    );
+    return res;
   }
 
   Future<List<ArtistModel>?> getArtists() async {
@@ -136,7 +172,7 @@ class SongProvider extends Baseprovider {
   Future<SongModel?> getRecent() async {
     try {
       final res = await _hiveService.getRecent();
-      _recent = _songList?.where((s) => res == s.id.toString()).toList().first;
+      _recent = _songList.where((s) => res == s.id.toString()).toList().first;
       notifyListeners();
       return _recent;
     } catch (e) {
@@ -144,60 +180,146 @@ class SongProvider extends Baseprovider {
     }
   }
 
-  Future<void> playSong(SongModel song) async {
-    await setRecent(song);
-    await loopMode();
-    if (song.title == _nowPlaying?.title && song.artist == _nowPlaying?.artist)
-      return;
-    isPlaying = true;
-    _nowPlaying = song;
-    _duration = await audioPlayer.setFilePath(File(song.data).path);
-    getPosition();
-    audioPlayer.play();
+  void setLoopMode(Loop loop) {
+    _currentLoop = loop;
     notifyListeners();
   }
 
-  Future<void> seek(double position) async {
-    await audioPlayer.seek(
-      Duration(seconds: currentDuration + position.toInt()),
-    );
+  Future<void> playSong(SongModel song) async {
+    if (audioPlayer?.playing == true) await disposePlayer();
+    try {
+      audioPlayer = AudioPlayer();
+      await setRecent(song);
+      isPlaying = true;
+      _nowPlaying = song;
+      _duration = await audioPlayer?.setFilePath(File(song.data).path);
+      final a = await audioQuery.queryArtwork(
+        song.id,
+        ArtworkType.AUDIO,
+        size: 800,
+      );
+      _nowPlayingArt = a;
+      audioPlayer?.play();
+      getPosition();
+      handleInterruptions();
+      notifyListeners();
+    } catch (e) {}
   }
 
-  Future<void> loopMode() async {
-    await audioPlayer.setLoopMode(LoopMode.one);
+  Future<void> seek(double position) async {
+    await audioPlayer?.seek(
+      Duration(seconds: currentDuration + position.toInt()),
+    );
   }
 
   Future<void>? pauseSong() async {
     isPlaying = false;
     notifyListeners();
-    return await audioPlayer.pause();
+    return await audioPlayer?.pause();
   }
 
   Future<void>? resume() async {
     isPlaying = true;
     notifyListeners();
-    return await audioPlayer.play();
+    return await audioPlayer?.play();
   }
 
   Future<void> stopPlaying() async {
     isPlaying = false;
     notifyListeners();
-    return await audioPlayer.stop();
+    return await audioPlayer?.stop();
+  }
+
+  void setPlayingList(List<SongModel> list, {bool clear = true}) async {
+    if (clear) _nowPlayingList.clear();
+    list.forEach((song) => _nowPlayingList.add(song));
+    notifyListeners();
+  }
+
+  void setShuffle(bool state) async {
+    _shuffleMode = state;
+    notifyListeners();
+  }
+
+  Future<void> disposePlayer() async {
+    isPlaying = false;
+    try {
+      await audioPlayer?.dispose();
+    } catch (e) {}
+    notifyListeners();
+  }
+
+  Future<void> skip({bool next = false, bool prev = false}) async {
+    List<SongModel> shuffled = [..._nowPlayingList];
+    try {
+      _currentIndex = _nowPlayingList.indexWhere(
+        (s) => s.id == _nowPlaying?.id,
+      );
+      await disposePlayer();
+      if (isRepeat == true) {
+        _nowPlaying = _nowPlaying;
+      } else if (_nowPlaying == shuffled.last) {
+        _nowPlaying = shuffled.first;
+      } else if (_shuffleMode == true) {
+        shuffled.shuffle();
+        _currentIndex =
+            shuffled.indexWhere((song) => song.id == _nowPlaying?.id);
+        _nowPlaying =
+            next ? shuffled[_currentIndex += 1] : shuffled[_currentIndex -= 1];
+      } else {
+        _nowPlaying =
+            next ? shuffled[_currentIndex += 1] : shuffled[_currentIndex -= 1];
+      }
+    } on RangeError catch (e) {
+      _nowPlaying = _nowPlaying;
+      print(e);
+    } catch (e) {
+      print("Error $e");
+    } finally {
+      await playSong(_nowPlaying!);
+      notifyListeners();
+    }
   }
 
   void getPosition() async {
-    audioPlayer.positionStream.listen(
+    audioPlayer?.positionStream.listen(
       (event) async {
-        _currentDuration = event.inSeconds;
-        _timePlayed = '${event.inMinutes}:${event.inSeconds % 60}';
-        notifyListeners();
+        if (event.inSeconds >= (_duration?.inSeconds ?? 0)) {
+          await skip(next: true);
+          // showNotification();
+        }
+        // notifyListeners();
       },
-    )..onError((error) => print('Error: $error'));
-    // ..onDone(() {
-    //   isPlaying = false;
-    //   _currentDuration = 0;
-    //   _timePlayed = '00:00';
-    //   notifyListeners();
-    // });
+    ).onError((error) => print('Error: $error'));
+  }
+
+  void handleInterruptions() {
+    AudioSession.instance.then((session) async {
+      audioPlayer?.playbackEventStream.listen((event) {
+        if (audioPlayer?.playing == true) {
+          session.setActive(true);
+        }
+      }).onError((e) => print(e));
+      session.interruptionEventStream.listen((event) {
+        if (event.begin) {
+          switch (event.type) {
+            case AudioInterruptionType.duck:
+            case AudioInterruptionType.pause:
+            case AudioInterruptionType.unknown:
+              pauseSong();
+              // showNotification();
+              break;
+            default:
+          }
+        } else {
+          switch (event.type) {
+            default:
+          }
+        }
+      });
+      session.becomingNoisyEventStream.listen((_) {
+        pauseSong();
+      });
+    });
   }
 }
