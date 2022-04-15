c31ce410d19aeab14b35790b0009920df8b5aabc
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2022-04-15 13:03:56 +0000

    [constants] add images for mixes

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,8 @@
 class Constants {
   static const imgpath = "images";
-
   static const IMG_DISK = "$imgpath/disk_image.png";
+  static const IMG_TAPES = "$imgpath/tapes.jpg";
+  static const IMG_HEADPHONE = "$imgpath/headphones.jpg";
+  static const IMG_GUITAR = "$imgpath/guitar.jpg";
+  static const IMG_CROWD = "$imgpath/crowd.jpg";
 }
